635c387617ded9a87f896d74365a7249f689bb4b
Biswa24 <sah.biswajeet@gmail.com>
2018-10-12 13:25:25 +0530

    code

---
+++ python.py
@@ -0,0 +1,23 @@
+MOD = 10**9 + 7
+
+class SegTree:
+    
+    def __init__(self, n):
+        self.N = 1 << n.bit_length()
+        self.tree = [0] * (self.N<<1)
+    
+    def update(self, i, j, v):
+        i += self.N
+        j += self.N
+        while i <= j:
+            if i%2==1: self.tree[i] += v
+            if j%2==0: self.tree[j] += v
+            i, j = (i+1) >> 1, (j-1) >> 1
+    
+    def query(self, i):
+        v = 0
+        i += self.N
+        while i > 0:
+            v += self.tree[i]
+            i >>= 1
+        return v
